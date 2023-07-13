53abaca6f0f9749519e3c861098a189142787bdf
Miguel Angel Condori Aroa <miguel.condori@csbp.com.bo>
2023-07-13 12:22:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## *Practica 2 -  Modulo 6 - VUE - USIP* 
+
+**Integrantes**
+
+ - Miguel Angel Condori Aroa
+
+-----------------------------------------
+### Ejecución del proyecto
+
+La practica se realizo con VUE 3 y Typescript
+
+1. Clonar el repositorio 
+
+    `https://github.com/miguel290280/practica2-vue-usip.git`
+
+2. Para levantar el servidor Json-Server, ingresar a la carpeta db y ejecutar
+
+    > json-server db.json --watch --port=3000
+
+3. Para ejecutar el programa ingresar a la carpeta
+
+    * Instalar los modulos ejecutar
+      > yarn
+
+    * Ejecutar el programa
+      > yarn dev
---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const configuracionPagina = ref<any>({
+    marca: "MegaDron",
+    menuColor: "lightblue",
+    menus: [
+        {
+            etiqueta: "Inicio",
+            url: "?"
+        },
+        {
+            etiqueta: "Tienda",
+            url: "?"
+        }
+    ]
+});
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg" :style="{ 'background-color': configuracionPagina.menuColor} ">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">{{ configuracionPagina.marca }}</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item"
+                      v-for="(menu, index) in configuracionPagina.menus"
+                      :key="index">
+                      <a class="nav-link" aria-current="page" href="#">{{ menu.etiqueta }}</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </nav>
+</template>
